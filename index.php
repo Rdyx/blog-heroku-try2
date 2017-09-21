ad94eccaee7d30380b892e6abc6f23174eeaa1df
Rdyx <tiranico.pro@gmail.com>
2017-09-21 21:08:00 +0200

    test

--- index.php
+++ index.php
@@ -12,12 +12,13 @@
  $dbconn = pg_connect('postgres://kcbynoltplfvnn:16b65a5cb5eb8f3dffcf23386e3854890484e7ba3874ec70db6df0411e0f8b6f@ec2-54-243-255-57.compute-1.amazonaws.com:5432/d2gh5poj295eo9');
 
   $result = pg_query($dbconn, 'select * from admin');
-  var_dump(pg_fetch_all($result));
 
   foreach($result as $value){
       echo $values[0];
     }
 
   ?>
+
+  <p>lzev</p>
 </body>
 </html>
