f50de0d66eeb7b489540ba684b5e88842a27d736
Rdyx <tiranico.pro@gmail.com>
2017-09-21 23:03:55 +0200

    test

--- web/pages/logged.php
+++ web/pages/logged.php
@@ -13,6 +13,8 @@
 	$pwd = strtoupper(hash('sha256', $_POST['pwd']));
 	echo "name : $row[0] <br>";
 	echo "test $pwd test ". $_POST['pwd'];
+	$row2 = pg_fetch_row($test);
+  echo "name : $row2[0]";
 
 	if($_POST['pseudo'] == $row[0] && $pwd == $row[1]){
 		$content = "GG";
