2ad59b2d713791b6e3d2ea492b74ed73351d7830
Rdyx <tiranico.pro@gmail.com>
2017-09-21 22:17:47 +0200

    test

--- web/layout/layout.php
+++ web/layout/layout.php
@@ -6,13 +6,19 @@
 	<title>Blog Randomz</title>
 </head>
 <body>
-	<nav class="navbar navbar-default">
+	<nav class="navbar navbar-inverse">
 		<div class="container-fluid">
 			<ul class="nav navbar-nav">
 				<li><a href="../../index.php">Link</a></li>
 				<li><a href="">Link</a></li>
 				<li><a href="">Link</a></li>
 			</ul>
+			<form action="" class="navbar-form navbar-left">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Recherche">
+				</div>
+				<button type="submit" class="btn btn-default">Rechercher</button>
+			</form>
 		</div>
 	</nav>
 	<h1>loalo</h1>
