3ce6dcb7293b24147fdc808c07e0835e66ee6309
Rdyx <tiranico.pro@gmail.com>
2017-09-21 19:10:52 +0200

    test

--- index.php
+++ index.php
@@ -6,5 +6,16 @@
 </head>
 <body>
   <h1>olololzerervzedvz</h1>
+
+  <?php 
+
+  $dbconn = pg_connect('postgres://kcbynoltplfvnn:16b65a5cb5eb8f3dffcf23386e3854890484e7ba3874ec70db6df0411e0f8b6f@ec2-54-243-255-57.compute-1.amazonaws.com:5432/d2gh5poj295eo9');
+
+  $result = pg_query($dbconn, 'select * from admin');
+  var_dump(pg_fetch_all($result));
+
+  include ('web/bdd/linkbdd.php');
+
+  ?>
 </body>
 </html>
