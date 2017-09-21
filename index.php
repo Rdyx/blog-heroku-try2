75f345a11bdd24114e58c7fdbc1fe647a1967a0f
Rdyx <tiranico.pro@gmail.com>
2017-09-22 01:01:29 +0200

    test

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
   $result = pg_query($dbconn, 'select * from articles');
 
   while($row = pg_fetch_row($result)){
-    $content .= '<div class="row text-center"><h1>$row[1]</h1></div>';
-    $content .= '<div class="row text-justify"><p>$row[2]<p></div>';
+    $content .= '<div class="row text-center"><h1> '.$row[1].' </h1></div>';
+    $content .= '<div class="row text-justify"><p> '.$row[2].' <p></div>';
   }
 
   include ('web/layout/layout.php');
