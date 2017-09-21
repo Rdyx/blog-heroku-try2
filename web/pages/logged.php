e905051fec42120a9443e598a56841e69d6fab20
Rdyx <tiranico.pro@gmail.com>
2017-09-21 22:54:57 +0200

    test

---
+++ web/pages/logged.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<head>
+	<meta charset="UTF-8">
+	<title>Post Article</title>
+</head>
+<body>
+	<?php include ('../layout/navbar.php');
+
+	$test = pg_query($dbconn, 'select * from admin');
+  $row = pg_fetch_row($test);
+  echo "name : $row2[0]";
+
+  if($_POST['pseudo'] == $row[0] && strtoupper(hash('sha256', $_POST['pwd'])) == $row[1]){
+  	$content = "GG";
+  } else {
+  	$content = "nope";
+  };
+
+  ?>
+
+</body>
+</html>
