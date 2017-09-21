e877f50adb7acbba850355a34e370891c90fe41a
Rdyx <tiranico.pro@gmail.com>
2017-09-21 22:47:50 +0200

    test

--- web/layout/navbar.php
+++ web/layout/navbar.php
@@ -3,7 +3,7 @@
 		<ul class="nav navbar-nav">
 		<li><a href="../../index.php">Accueil</a></li>
 			<li><a href="">Link</a></li>
-			<li><a href="web/pages/login.php">Login</a></li>
+			<li><a href="/web/pages/login.php">Login</a></li>
 		</ul>
 		<form action="../../index.php" class="navbar-form navbar-left">
 			<div class="form-group">
