4873ae527753648db00ead0a809cdf64f328139c
Rdyx <tiranico.pro@gmail.com>
2017-09-22 10:44:06 +0200

    test

--- index.php
+++ index.php
@@ -34,9 +34,9 @@
 
   if($id == ""){
   } else {
-    $selection = pg_query($dbconn, "SELECT art_oid FROM articles WHERE art_oid LIKE '".$id."'");
+    $selection = pg_query($dbconn, "SELECT * FROM articles WHERE art_oid LIKE '".$id."'");
     $row = pg_fetch_row($selection);
-    $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
+    $content = '<div class="row"><h1> '.$row[1].' </h1></div>';
     $content .= '<div class="row"><p> '.$row[3].' <p></div>';
   }
 
