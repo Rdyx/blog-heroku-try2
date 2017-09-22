34dcb1f46ec3ef867107ce0a2bd0bf169d4fcedd
Rdyx <tiranico.pro@gmail.com>
2017-09-22 12:22:59 +0200

    test

--- index.php
+++ index.php
@@ -15,42 +15,27 @@
   $searchInput = htmlspecialchars($_POST['search']);
 
   $result = pg_query($dbconn, 'select * from articles');
-  boucle($result);
+  $content = boucle($result);
   
-  if($searchInput == ""){
-    $result = pg_query($dbconn, 'select * from articles');
-} else {
+  if(!$searchInput == ""){
   $search = pg_query($dbconn, "SELECT * FROM articles WHERE art_title LIKE '%".$searchInput."%' OR art_content LIKE '%".$searchInput."%'");
-  boucle($search);
+  $content = boucle($search);
 }
 
 $id = htmlspecialchars($_GET['id']);
 
-if($id == ""){
-} else {
+if(!$id){
   $selection = pg_query($dbconn, "SELECT * FROM articles WHERE art_oid = '".$id."'");
-  $row = pg_fetch_row($selection);
-  $content = '<div class="row"><h1> '.$row[1].' </h1></div>';
-  $content .= '<div class="row"><p> '.$row[3].' <p></div>';
-  $content .= '<div class="row text-right"><a href="?genre='.$row[4].'">Thème : '.$row[4].'</a></div>';
-}
+  $content = boucle($id);  
+};
+
 
 $genre = htmlspecialchars($_GET['genre']);
 
 if($genre == ""){
-} else {
   $selection = pg_query($dbconn, "SELECT * FROM articles WHERE art_genre LIKE '".$genre."'");
-  $row = pg_fetch_row($selection);
-  $content = '<div class="row"><h1> '.$row[1].' </h1></div>';
-  $content .= '<div class="row"><p> '.$row[3].' <p></div>';
-  $content .= '<div class="row text-right">
-                  <ul class="list-inline">
-                    <li><a href="?genre='.$row[4].'">Thème : '.$row[4].'</a></li>
-                    <li> - </li>
-                    <li><a href="?id='.$row[0].'">Voir les commentaires</a></li>
-                  </ul>
-                </div>';
-}
+  $content = boucle($genre);  
+};
 
 function boucle($arg1){
   while($row = pg_fetch_row($arg1)){
