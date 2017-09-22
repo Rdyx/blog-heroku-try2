42c810f96ba4299fa1cd062e74cf27e1a34b94db
Rdyx <tiranico.pro@gmail.com>
2017-09-22 10:38:21 +0200

    test

--- index.php
+++ index.php
@@ -27,7 +27,16 @@
     while($row = pg_fetch_row($search)){
       $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
       $content .= '<div class="row"><p> '.$row[3].' <p></div>';
+      $content .= '<div class="row"><a href="?id='.$row[0].'"><h1>#'.$row[0].'</h1></a></div>';
     }
+  }
+
+  if($_GET['id'] == ""){
+  } else {
+    $selection = pg_query($dbconn, "SELECT art_oid FROM articles WHERE art_oid LIKE '".$_GET['id']."'"));
+    $row = pg_fetch_row($selection);
+    $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
+      $content .= '<div class="row"><p> '.$row[3].' <p></div>';
   }
 
   include ('web/layout/layout.php');
