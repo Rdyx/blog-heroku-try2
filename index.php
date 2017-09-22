c7548608b05e50c341067d95cf8dbf39275d1397
Rdyx <tiranico.pro@gmail.com>
2017-09-22 10:30:58 +0200

    test

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     while($row = pg_fetch_row($result)){
       $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
       $content .= '<div class="row"><p> '.$row[3].' <p></div>';
-      $content .= '<a href="?id='.$row[0].'><h1>#'.$row[0].'</h1></a>';
+      $content .= '<div class="row"><a href="?id='.$row[0].'><h1>#'.$row[0].'</h1></a></div>';
     }
   } else {
     $search = pg_query($dbconn, "SELECT * FROM articles WHERE art_title LIKE '%".$searchInput."%' OR art_content LIKE '%".$searchInput."%'");
