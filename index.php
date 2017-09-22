023114c1c7950b85aae914adf2457c03d0e55367
Rdyx <tiranico.pro@gmail.com>
2017-09-22 10:06:50 +0200

    test

--- index.php
+++ index.php
@@ -15,20 +15,19 @@
   $searchInput = htmlspecialchars($_POST['search']);
   echo $searchInput;
 
-  // if($searchInput == ""){
-  //   $result = pg_query($dbconn, 'select * from articles');
-  //   while($row = pg_fetch_row($result)){
-  //     $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
-  //     $content .= '<div class="row"><p> '.$row[3].' <p></div>';
-  //   }
-  // } else {
-    $search = "SELECT * FROM articles WHERE art_title LIKE '%%%s%%'";
-    pg_query($dbconn, $search);
+  if($searchInput == ""){
+    $result = pg_query($dbconn, 'select * from articles');
+    while($row = pg_fetch_row($result)){
+      $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
+      $content .= '<div class="row"><p> '.$row[3].' <p></div>';
+    }
+  } else {
+    $search = pg_query($dbconn, "SELECT * FROM articles WHERE art_title LIKE '%".$searchInput."%'");
     while($row = pg_fetch_row($search)){
       $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
       $content .= '<div class="row"><p> '.$row[3].' <p></div>';
    }
-  // }
+  }
 
 
   include ('web/layout/layout.php');
