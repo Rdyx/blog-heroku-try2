44b9bae5bac74b04a71a57d3e397cf48911f923a
Rdyx <tiranico.pro@gmail.com>
2017-09-22 12:13:11 +0200

    test

--- index.php
+++ index.php
@@ -29,17 +29,18 @@
   }
 } else {
   $search = pg_query($dbconn, "SELECT * FROM articles WHERE art_title LIKE '%".$searchInput."%' OR art_content LIKE '%".$searchInput."%'");
-  while($row = pg_fetch_row($search)){
-    $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
-    $content .= '<div class="row"><p> '.$row[3].' <p></div>';
-    $content .= '<div class="row text-right">
-                  <ul class="list-inline">
-                    <li><a href="?genre='.$row[4].'">Thème : '.$row[4].'</a></li>
-                    <li> - </li>
-                    <li><a href="?id='.$row[0].'">Voir les commentaires</a></li>
-                  </ul>
-                </div>';
-}
+  $content .= boucle($search);
+//   while($row = pg_fetch_row($search)){
+//     $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
+//     $content .= '<div class="row"><p> '.$row[3].' <p></div>';
+//     $content .= '<div class="row text-right">
+//                   <ul class="list-inline">
+//                     <li><a href="?genre='.$row[4].'">Thème : '.$row[4].'</a></li>
+//                     <li> - </li>
+//                     <li><a href="?id='.$row[0].'">Voir les commentaires</a></li>
+//                   </ul>
+//                 </div>';
+// }
 }
 
 $id = htmlspecialchars($_GET['id']);
@@ -69,6 +70,21 @@ if($genre == ""){
                   </ul>
                 </div>';
 }
+
+function boucle($arg1){
+  while($row = pg_fetch_row($arg1)){
+    $content .= '<div class="row"><h1> '.$row[1].' </h1></div>';
+    $content .= '<div class="row"><p> '.$row[3].' <p></div>';
+    $content .= '<div class="row text-right">
+                <ul class="list-inline">
+                  <li><a href="?genre='.$row[4].'">Thème : '.$row[4].'</a></li>
+                  <li> - </li>
+                  <li><a href="?id='.$row[0].'">Voir les commentaires</a></li>
+                </ul>
+              </div>';
+}
+}
+
 
 include ('web/layout/layout.php');
 
