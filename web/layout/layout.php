6fd631fe425f22b95f1eca10e56a80abbae83fa5
Rdyx <tiranico.pro@gmail.com>
2017-09-23 01:23:09 +0200

    test

--- web/layout/layout.php
+++ web/layout/layout.php
@@ -1,5 +1,7 @@
 <div class="container text-center well well-sm" id="list-articles">
-	<ul class="pagination"></ul>
+	<div class="col-xs-12">
+		<ul class="pagination"></ul>
+	</div>
 	<div class="col-xs-offset-1 col-xs-10 list">
 		<?php echo $content ?>
 	</div>
