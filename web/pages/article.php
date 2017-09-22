d3e1cbb0b02f461f7b35f4c75700ed17bfeef2d2
Rdyx <tiranico.pro@gmail.com>
2017-09-22 11:26:36 +0200

    test

--- web/pages/article.php
+++ web/pages/article.php
@@ -17,7 +17,7 @@
     if($id == ""){
     } else {
       $selection = pg_query($dbconn, "SELECT * FROM articles WHERE art_oid = '".$id."'");
-      $row = pg_fetch_row($selection)
+      $row = pg_fetch_row($selection);
         $content = '<div class="row"><h1> '.$row[1].' </h1></div>';
         $content .= '<div class="row"><p> '.$row[3].' <p></div>';
     }
