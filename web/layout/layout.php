a1a68f02970e1e0d352a962bf94881618a6c6cfe
Rdyx <tiranico.pro@gmail.com>
2017-09-22 23:39:41 +0200

    test

--- web/layout/layout.php
+++ web/layout/layout.php
@@ -1,4 +1,4 @@
-<div class="container text-center well well-sm list-articles">
+<div class="container text-center well well-sm" id="list-articles">
 	<div class="col-xs-offset-1 col-xs-10 list">
 		<?php echo $content ?>
 	</div>
