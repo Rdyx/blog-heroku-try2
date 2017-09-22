3276e7cfdd13e9f6628d5d022b0563112a0dac62
Rdyx <tiranico.pro@gmail.com>
2017-09-22 10:48:37 +0200

    test

--- index.php
+++ index.php
@@ -35,9 +35,10 @@
   if($id == ""){
   } else {
     $selection = pg_query($dbconn, "SELECT * FROM articles WHERE art_oid LIKE '".$id."'");
-    $row = pg_fetch_row($selection);
-    $content = '<div class="row"><h1> '.$row[1].' </h1></div>';
-    $content .= '<div class="row"><p> '.$row[3].' <p></div>';
+    while($row = pg_fetch_row($selection)){;
+      $content = '<div class="row"><h1> '.$row[1].' </h1></div>';
+      $content .= '<div class="row"><p> '.$row[3].' <p></div>';
+    }
   }
 
   include ('web/layout/layout.php');
