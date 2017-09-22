908e688f1c76383e96078538e417fb53e5499d71
Rdyx <tiranico.pro@gmail.com>
2017-09-23 01:16:13 +0200

    test

--- web/pages/liste.php
+++ web/pages/liste.php
@@ -93,7 +93,7 @@
   src="https://code.jquery.com/jquery-3.2.1.js"
   integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
   crossorigin="anonymous"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
+<!-- <script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script> -->
 <script src="../js/table-list.js"></script>
 </body>
 </html>
