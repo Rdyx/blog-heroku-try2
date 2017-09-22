f8e8d7356b167da1ff99ba9ac18b573a94726f01
Rdyx <tiranico.pro@gmail.com>
2017-09-22 22:34:52 +0200

    test

--- web/layout/layout.php
+++ web/layout/layout.php
@@ -1,7 +1,5 @@
 <div class="container text-center well well-sm">
 	<div class="col-xs-offset-1 col-xs-10">
-		<div class="container well well-sm">
-			<?php echo $content ?>
-		</div>
+		<?php echo $content ?>
 	</div>
 </div>
