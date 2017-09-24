afdd169fa2c44944d43cccab735406ae5aa9a9d6
Rdyx <tiranico.pro@gmail.com>
2017-09-24 19:27:39 +0200

    test

--- web/layout/navbar.php
+++ web/layout/navbar.php
@@ -3,17 +3,29 @@
 		<ul class="nav navbar-nav navbar-left">
 			<li><a href="../../index.php">Accueil</a></li>
 			<li><a href="/web/pages/liste.php">Liste des articles</a></li>
+
+		<?php
+			if (!isset($_SESSION['nickname'])) { ?>
 			<li><a href="/web/pages/login.php">Login</a></li>
-		</ul>
-		<ul class="navbar-right">
+		
+		<?php
+			} else { 
+		?>
+			<li><a href="/web/pages/disconnect.php">Se déconnecter</a></li>
+		
+		<?php
+			};
+		?>
+	</ul>
+	<ul class="nav navbar-nav navbar-right">
 		<li><a>Bienvenue <?= $_SESSION['nickname']; ?> !</a></li>
 		<li>
-		<form action="../../index.php" method="post" class="navbar-form">
-			<div class="form-group">
-				<input type="text" class="form-control" name="search" id="search" placeholder="Rechercher dans les articles..." size="35">
-			</div>
-			<button type="submit" class="btn btn-default">Rechercher</button>
-		</form>
+			<form action="../../index.php" method="post" class="navbar-form">
+				<div class="form-group">
+					<input type="text" class="form-control" name="search" id="search" placeholder="Rechercher dans les articles..." size="35">
+				</div>
+				<button type="submit" class="btn btn-default">Rechercher</button>
+			</form>
 		</li>
 	</div>
 </nav>
---
+++ web/pages/disconnect.php
@@ -0,0 +1,34 @@
+<?php
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+
+  include('../layout/session.php');
+  include ('../layout/navbar.php');
+
+  $id = htmlspecialchars($_GET['id']);
+
+  if($id == ""){
+  } else {
+    $selection = pg_query($dbconn, "SELECT * FROM articles WHERE art_oid = '".$id."'");
+    $row = pg_fetch_row($selection);
+    $content = '<div class="row"><h1> '.$row[1].' </h1></div>';
+    $content .= '<div class="row"><p> '.$row[3].' <p></div>';
+  }
+
+  include ('../layout/layout.php');
+
+  ?>
+
+</body>
+</html>
