5b0ddfa8e7acd2de6e8bc766c58bc2341df43e64
Rdyx <tiranico.pro@gmail.com>
2017-09-24 20:37:20 +0200

    test

--- index.php
+++ index.php
@@ -51,6 +51,7 @@
     $rowLog = pg_fetch_row($connect);
 
     while($row = pg_fetch_row($arg1)){
+      var_dump($rowLog[0]);
       $content .= '<div class="row well well-lg article"><div class="row"><h1><strong> '.$row[1].' </strong></h1></div>';
       $content .= '<div class="row text-justify well"><p> '.$row[3].' <p></div>';
       $content .= '<div class="row text-center">';
