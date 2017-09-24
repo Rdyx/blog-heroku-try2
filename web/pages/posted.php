0a64751f9a6ee69c6b7b7cbcdf362c56d36b48a5
Rdyx <tiranico.pro@gmail.com>
2017-09-24 23:28:15 +0200

    test

--- web/pages/posted.php
+++ web/pages/posted.php
@@ -21,10 +21,13 @@
 
 
 		if(empty($artTitle) || empty($artContent) || strlen($artTitle) > 50 || strlen($artContent) > 1000 || $artMois <= 0 || $artMois > 12 || empty($artMois) || $artAnnee <= -10001 || $artAnnee > date('Y') || empty($artAnnee)){
-			$content = '<div class="row"><h1>Erreur !</h1> <br> <a href="login.php">Retour</a></div>';
+			$content = '<div class="row"><h1>Erreur !</h1> <br> <a href="post.php">Retour</a></div>';
 		} else {
 			pg_query($dbconn, "INSERT INTO articles (art_title, art_content, art_genre, art_month, art_year, art_date, art_autor) VALUES ('".$artTitle."', '".$artContent."', '".$artGenre."', '".$artMois."', '".$artAnnee."', '".date('d-m-Y à H:i')."', '".$_SESSION['nickname']."')");
-			$content = '<div class="row"><h1>Success !</h1> <br> <a href="../../index.php">Retour à l\'index</a></div>';
+			header("refresh:5; url=../../index.php");
+			$content = '<div class="row"><h1>Success !</h1><br>
+						<p>Patientez 5 secondes ou cliquez sur le lien ci-dessous pour revenir à la page d\'accueil.</p></br>
+					<a href="../../index.php"><p>Retour à l\'accueil</p></a></div>';';
 		};
 		
 		include ('../layout/layout.php');
