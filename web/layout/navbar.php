48c3a1f7ad7347a849c5ebd561afac5e956cc6af
Rdyx <tiranico.pro@gmail.com>
2017-09-24 15:48:07 +0200

    test

--- web/layout/navbar.php
+++ web/layout/navbar.php
@@ -7,7 +7,7 @@
 		</ul>
 		<form action="../../index.php" method="post" class="navbar-form navbar-right">
 			<div class="form-group">
-				<input type="text" class="form-control" name="search" id="search" placeholder="Rechercher dans les articles..." size="75">
+				<input type="text" class="form-control" name="search" id="search" placeholder="Rechercher dans les articles..." size="35">
 			</div>
 			<button type="submit" class="btn btn-default">Rechercher</button>
 		</form>
--- web/layout/navbar_list.php
+++ web/layout/navbar_list.php
@@ -7,7 +7,7 @@
 		</ul>
 		<form action="liste.php" method="post" class="navbar-form navbar-right">
 			<div class="form-group">
-				<input type="text" class="form-control" name="search" id="search" placeholder="Rechercher un titre dans la liste..." size="75">
+				<input type="text" class="form-control" name="search" id="search" placeholder="Rechercher un titre dans la liste..." size="35">
 			</div>
 			<button type="submit" class="btn btn-default">Rechercher</button>
 		</form>
