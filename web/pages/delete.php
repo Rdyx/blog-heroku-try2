605532dd33a6c3675353f4d4d3292f31e3dc3625
Rdyx <tiranico.pro@gmail.com>
2017-09-25 12:02:18 +0200

    test

---
+++ web/pages/delete.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<head>
+	<meta charset="UTF-8">
+	<title>Post Article</title>
+</head>
+<body>
+	<?php
+
+	include('../layout/session.php');
+	include ('../layout/navbar.php');
+
+	$artId = $_GET['id'];
+	$connect = pg_query($dbconn, "SELECT adm_name FROM admin");
+	$rowLog = pg_fetch_row($connect);
+
+	if($_SESSION['nickname'] == $rowLog[0] || $_SESSION['nickname'] == $row[8]){
+		$select = pg_query($dbconn, "DELETE FROM articles WHERE art_oid = ".$artId);
+
+		header("refresh:5; url=../../index.php");
+
+		$content = '<div class="row"><h1>Article supprimé !</h1></br>
+					<p>Votre article "<strong>'.$row[1].'</strong>" a bien été supprimé.</p></br>
+					<p>Patientez 5 secondes ou cliquez sur le lien ci-dessous pour revenir à la page d\'accueil.</p></br>
+					<a href="../../index.php"><p>Retour à l\'accueil</p></a></div>';
+	} else {
+		$content .= '<div class="row"><h1>Erreur !</h1><br>
+					<p>Une erreur est survenue !</p><br>
+					<a href="../../index.php"><p>Retour à l\'accueil</p></a></div>';
+	}
+
+	include ('../layout/layout_nolist.php');
+
+	?>
+
+</body>
+</html>
