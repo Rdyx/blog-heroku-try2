2102b7339191ea467594a986c537f41551928540
Rdyx <tiranico.pro@gmail.com>
2017-09-26 01:02:37 +0200

    test

--- web/layout/comments.php
+++ web/layout/comments.php
@@ -4,25 +4,26 @@
   $id = htmlspecialchars($_GET['id']);
   var_dump($id);
 
+  $content .= '<div class="row well" id="listComs"><h3>Commentaires</h3>
+  <div class="col-xs-12">
+  	<ul class="pagination"></ul>
+  </div>
+  <div class="row list">';
+  	
   function boucleCom($arg1, $arg2){
-  	$content .= '<div class="row" id="listComs"><h3>Commentaires</h3>
-  				<div class="col-xs-12">
-					<ul class="pagination"></ul>
-				</div>'
-  	$content .= '<div class="row list">';
   	while($row = pg_fetch_row($arg1)){
   		$content .= '<div class="col-xs-offset-1 col-xs-10 well well-lg comment"><p>'.$row[0].'</p></div>';
 
-      if($_SESSION['nickname'] == $arg2){
-      	//Add com autor later
-      	//|| $_SESSION['nickname'] == $row[8]){
-        $content .= '<div class="col-xs-12 text-justify">
-                    <ul class="list-inline">
-                      <li><a href="modify.php?id='.$row[0].'">Modifier</a></li>
-                      <li><a href="delete.php?id='.$row[0].'">Supprimer</a></li>
-                    </ul>
-                    </div>';
-      };
+      // if($_SESSION['nickname'] == $arg2){
+      // 	//Add com autor later
+      // 	//|| $_SESSION['nickname'] == $row[8]){
+      //   $content .= '<div class="col-xs-12 text-justify">
+      //               <ul class="list-inline">
+      //                 <li><a href="modify.php?id='.$row[0].'">Modifier</a></li>
+      //                 <li><a href="delete.php?id='.$row[0].'">Supprimer</a></li>
+      //               </ul>
+      //               </div>';
+      // };
   	}
       $content.= '</div>
       			<div class="col-xs-offset-1 col-xs-10 well text-justify"><p><strong>Ecrire un commentaire</strong></p>
