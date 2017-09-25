44640b1abaf2722b1e22f5ba16c6b76693596d4b
Rdyx <tiranico.pro@gmail.com>
2017-09-25 23:55:51 +0200

    test

--- web/layout/comments.php
+++ web/layout/comments.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<head>
-  <meta charset="UTF-8">
-  <title>Document</title>
-</head>
-<body>
-
-  <?php
-
-  include ('../layout/session.php');
-  include ('../layout/navbar.php');
-
+<?php
   $comsResult = pg_query($dbconn, "SELECT * FROM comments ORDER BY com_oid DESC");
   $connect = pg_query($dbconn, "SELECT adm_name FROM admin");
   $rowLog = pg_fetch_row($connect);
@@ -48,10 +35,4 @@
     return $content;
   }
 
-  include ('../layout/layout.php');
-
 ?>
-
-
-</body>
-</html>
