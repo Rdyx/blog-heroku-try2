f299bc034c242c8bebe797eadc5135afb54df9c2
Rdyx <tiranico.pro@gmail.com>
2017-09-25 12:13:38 +0200

    test

--- web/pages/delete.php
+++ web/pages/delete.php
@@ -11,12 +11,12 @@
 	include('../layout/session.php');
 	include ('../layout/navbar.php');
 
-	$artId = $_GET['id'];
+	$id = $_GET['id'];
 	$connect = pg_query($dbconn, "SELECT adm_name FROM admin");
 	$rowLog = pg_fetch_row($connect);
 
 	if($_SESSION['nickname'] == $rowLog[0] || $_SESSION['nickname'] == $row[8]){
-		pg_query($dbconn, "DELETE FROM articles WHERE art_oid = '".$artId."'");
+		pg_query($dbconn, "DELETE FROM articles WHERE art_oid = '".$id."'");
 
 		header("refresh:5; url=../../index.php");
 		$content .= '<div class="row"><h1>Article supprimé !</h1></br>
