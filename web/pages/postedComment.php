d7595970a2322149b29ad7f6970d4e7fddee6787
Rdyx <tiranico.pro@gmail.com>
2017-09-26 01:17:37 +0200

    test

--- web/pages/postedComment.php
+++ web/pages/postedComment.php
@@ -22,7 +22,7 @@
 			header("refresh:5; url=article.php?id=".$id);
 			$content = '<div class="row"><h1>Success !</h1><br>
 						<p>Patientez 5 secondes ou cliquez sur le lien ci-dessous pour revenir à l\'article.</p></br>
-					<a href="article.php?id='.$id.'><p>Retour à l\'article</p></a></div>';
+					<a href="article.php?id='.$id.'"><p>Retour à l\'article</p></a></div>';
 		};
 
 		include ('../layout/layout_nolist.php');
