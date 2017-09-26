331a06f9ed1876a8a49dc1e9e5f1b8bf9a6e901a
Rdyx <tiranico.pro@gmail.com>
2017-09-26 11:04:03 +0200

    test

--- web/layout/comments.php
+++ web/layout/comments.php
@@ -1,5 +1,5 @@
 <?php
-  $comsResult = pg_query($dbconn, "SELECT com_content FROM comments WHERE art_com = '$id'");
+  $comsResult = pg_query($dbconn, "SELECT com_content FROM comments WHERE art_oid = '$id'");
 
     $content .= '<div class="row well" id="listComs"><h3>Commentaires</h3>
   <div class="col-xs-12">
